1a23796dc17a9fef445a02b5569301bf2bbecec9
Andrii Korol <aekorol777@gmail.com>
2019-03-05 08:31:09 +0200

    Updates

---
+++ precache-manifest.498c56519f5cb473116b8d17a6a46ad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d921c0b405b0490bcdfd",
+    "url": "/video-links-helper/static/js/runtime~main.d921c0b4.js"
+  },
+  {
+    "revision": "feea250b85a7b62fa431",
+    "url": "/video-links-helper/static/js/main.feea250b.chunk.js"
+  },
+  {
+    "revision": "bc5ad4bcf8336e397813",
+    "url": "/video-links-helper/static/js/2.bc5ad4bc.chunk.js"
+  },
+  {
+    "revision": "feea250b85a7b62fa431",
+    "url": "/video-links-helper/static/css/main.125d0bf8.chunk.css"
+  },
+  {
+    "revision": "430291ebe3051069792aa8ca710d0013",
+    "url": "/video-links-helper/index.html"
+  }
+];
